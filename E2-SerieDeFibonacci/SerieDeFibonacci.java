636a062e280ec1b4fa730b6a7e35c8612080dd72
droare <droare@gmail.com>
2022-08-17 08:29:21 -0500

    Ejercicio Fibonacci

---
+++ E2-SerieDeFibonacci/SerieDeFibonacci.java
@@ -0,0 +1,21 @@
+//package E2-SerieDeFibonacci;
+import java.util.Scanner;
+
+public class SerieDeFibonacci {
+
+int tamSerie;
+int serifibonacci [] = new int[tamSerie];
+
+public void TamanioSerie(){
+
+System.out.print ("Cuantos elementos de la serie deseas que se muestren: ");
+Scanner Scannum = new Scanner (System.in);
+tamSerie = Scannum.nextInt();   
+Scannum.close();
+
+}
+
+
+
+    
+}
